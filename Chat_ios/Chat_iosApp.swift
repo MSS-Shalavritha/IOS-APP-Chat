a594a1ee2f39142273c4f1662942611d244ab292
shalavritha mamunooru <shalavritha.srtforever@gmail.com>
2024-06-07 00:51:58 -0700

    Initial Commit

---
+++ Chat_ios/Chat_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_iosApp.swift
+//  Chat_ios
+//
+//  Created by Shalavritha Mamunooru on 07/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Chat_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
